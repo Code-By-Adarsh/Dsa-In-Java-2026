2a126fea226054ad7313c85faabb1086d7cf0464
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-01-19 12:28:26 +0530

    Add: Complete Day-2

--- Day_02/Day02.java
+++ Day_02/Day02.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Day02 {
     public static void main(String[] args) {
-        // 1st
+        // 1st - Arithmetic operator
         System.out.println(" ");
         double x = 5, y = 2;
         System.out.println(x+y);
@@ -17,10 +19,12 @@ public class Day02 {
         // 3rd HW - Volume of sphere
         System.out.println(" ");
         double r1 = 3.5;
-        double v = 4/3*Math.PI*r1*r1*r1;
+        double v = (4.0/3)*Math.PI*Math.pow(r1,3);
+        double v1 = 4.0/3*Math.PI*Math.pow(r1,3);
         System.out.println(v);
+        System.out.println(v1);
 
-        // 4rth - Total surface area of cuboid
+        // 4rth HW - Total surface area of cuboid
         System.out.println(" ");
         double l, b, h, ans;
         l = 2.5;
@@ -34,5 +38,57 @@ public class Day02 {
         // Rule 2 - Numbers accepted but in middle and end
         // Rule 3 - No any special characters allowed expect _ and $
         // Rule 4 - Spaces and commas are not allowed in variable
+
+        // 6th - Scanner
+        System.out.println(" ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the radius of circle: ");
+        double r2 = sc.nextDouble();
+        double area = Math.PI*Math.pow(r2,2);
+        System.out.printf("Area of circle of radius %.1f : "+area,r2);
+
+        // 7th - Printing square
+        System.out.println(" ");
+        System.out.print("\nEnter your number: ");
+        double n = sc.nextDouble();
+        System.out.printf("Square of %.1f : "+n*n,n);
+
+        // 8th - Simple interest
+        System.out.println(" ");
+        System.out.print("\nEnter principal: ");
+        double p = sc.nextDouble();
+        System.out.print("Enter rate: ");
+        double r3 = sc.nextDouble();
+        System.out.print("Enter time: ");
+        double t = sc.nextDouble();
+        System.out.println("Simple Interest: "+(p*r3*t)/100);
+
+        // 9th - Modulus operator
+        System.out.println(" ");
+        int a1 = 9;
+        int b1 = 4;
+        System.out.println(a1%b1);
+
+        // 10th - Int/Double concept
+        System.out.println(" ");
+        double a2 = 5/2;
+        double a3 = 5.0/2;
+        System.out.println(a2);
+        System.out.println(a3);
+
+        // 11th - Char data type
+        System.out.println(" ");
+        char adarsh = 'R';
+        System.out.println(adarsh);
+        char adarsh1 = '$';
+        System.out.println(adarsh1);
+
+        // 12th - Typecasting : ek data type se dusre data type conversion
+        System.out.println(" ");
+        char ch = 'A';
+        int a4 = ch; // Implicit typecasting
+        System.out.println(a4);
+        char ch1 = '$';
+        System.out.println((int)ch1); // Explicit typecasting
     }
 }
