9914c5412c2774a87c70334478030ad3402eca86
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-02-23 13:57:40 +0530

    Add: Day-37

---
+++ Day_37/Day37.java
@@ -0,0 +1,123 @@
+public class Day37 {
+    public static void print(int[] arr){
+        for (int ele:arr){
+            System.out.print(ele+" ");
+        }
+        System.out.println(" ");
+    }
+
+    public static void isArraySorted(int[] arr){
+        for (int i=0; i<arr.length-1; i++){
+            if (arr[i] > arr[i+1]){
+                System.out.println("False");
+                break;
+            }
+        }
+        System.out.println("True");
+    }
+
+    public static void main(String[] args) {
+
+        // 1st - Check array is sorted
+        System.out.println(" ");
+        int[] x1 = {1,1,2,3,4,4,5,6};
+        System.out.print("Given Array: ");
+        print(x1);
+        System.out.print("Array is sorted: ");
+        isArraySorted(x1);
+
+        // 2nd - Bubble sort (basic)
+        System.out.println(" ");
+        int[] x2 = {5,1,4,3,0,-1,1,2};
+        int n = x2.length;
+        System.out.print("Given Array: ");
+        print(x2);
+        for (int i=0; i<n-1; i++){
+            for (int j=0; j<n-1-i; j++){
+                if (x2[j] > x2[j+1]){
+                    int temp = x2[j];
+                    x2[j] = x2[j+1];
+                    x2[j+1] = temp;
+                }
+            }
+        }
+        System.out.print("Sorted Array: ");
+        print(x2);
+
+        // 3rd - Bubble sort (basic+)
+        System.out.println(" ");
+        int[] x3 = {5,1,4,3,0,-1,1,2};
+        int n3 = x3.length;
+        System.out.print("Given Array: ");
+        print(x3);
+        for (int i=0; i<n3-1; i++){
+            boolean isSorted = true;
+            for (int j=0; j<n3-1; j++){
+                if (x3[j] > x3[j+1]){
+                    isSorted = false;
+                    break;
+                }
+            }
+            if (isSorted){
+                break;
+            }else{
+                for (int j=0; j<n3-1-i; j++){
+                    if (x3[j] > x3[j+1]){
+                        int temp = x3[j];
+                        x3[j] = x3[j+1];
+                        x3[j+1] = temp;
+                    }
+                }
+            }
+        }
+        System.out.print("Sorted Array: ");
+        print(x3);
+
+        // 4rth - Bubble sort (basic++)
+        System.out.println(" ");
+        int[] x4 = {5,1,4,3,0,-1,1,2};
+        int n4 = x4.length;
+        System.out.print("Given Array: ");
+        print(x4);
+        for (int i=0; i<n4-1; i++){
+            boolean isSorted = true;
+            for (int j=0; j<n4-1-i; j++) {
+                if (x4[j] > x4[j + 1]) {
+                    int temp = x4[j];
+                    x4[j] = x4[j + 1];
+                    x4[j + 1] = temp;
+                    isSorted = false;
+                }
+            }
+            if (isSorted){
+                break;
+            }
+        }
+        System.out.print("Sorted Array: ");
+        print(x4);
+
+        // 5th - HW - Bubble sort in descending order
+        System.out.println(" ");
+        int[] x5 = {5,3,1,4,9,0,3,2,-1,7};
+        System.out.print("Given Array: ");
+        print(x5);
+        int n5 = x5.length;
+        for (int i=0; i<n5-1; i++){
+            int swap = 0;
+            for (int j = n5-1; j>i; j--){
+                if (x5[j]>x5[j-1]){
+                    int temp = x5[j];
+                    x5[j] = x5[j-1];
+                    x5[j-1] = temp;
+                    swap++;
+                }
+            }
+            if (swap == 0){
+                break;
+            }
+        }
+
+        System.out.print("Sorted Array in desc order: ");
+        print(x5);
+    }
+}
