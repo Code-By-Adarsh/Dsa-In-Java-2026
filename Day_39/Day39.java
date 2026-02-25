f04991ffe727d8279f6290feedc91797b8e8674f
Code-By-Adarsh <adarshmishra70931@gmail.com>
2026-02-26 00:02:26 +0530

    Dont know

--- Day_39/Day39.java
+++ Day_39/Day39.java
@@ -1,7 +1,7 @@
 public class Day39 {
-    public static void print(int[] arr){
-        for(int ele:arr){
-            System.out.print(ele+" ");
+    public static void print(int[] arr) {
+        for (int ele : arr) {
+            System.out.print(ele + " ");
         }
         System.out.println(" ");
     }
@@ -10,14 +10,14 @@ public class Day39 {
 
         // 1st - Selection sorting descending order
         System.out.println(" ");
-        int[] x1 = {2,0,5,8,2,1,6,4};
+        int[] x1 = {2, 0, 5, 8, 2, 1, 6, 4};
         System.out.print("Given Array: ");
         print(x1);
 
-        for(int i=0; i<x1.length; i++){
+        for (int i = 0; i < x1.length; i++) {
             int maxdex = i;
-            for (int j=i+1; j<x1.length; j++){
-                if (x1[maxdex] < x1[j]){
+            for (int j = i + 1; j < x1.length; j++) {
+                if (x1[maxdex] < x1[j]) {
                     maxdex = j;
                 }
             }
